5e00d1ec7b8cf6b74ac9d8fa2ab91cca11a9e2ee
Daniele Pusceddu <43929743+danielepusceddu@users.noreply.github.com>
2023-05-21 15:33:45 +0200

    Supply: redefine using curried swaps (account sets) and new AMM Sets

--- Supply.lean
+++ Supply.lean
@@ -6,26 +6,44 @@ open BigOperators
 
 def pick2 {α β: Type} (_: α) (x: β) := x
 
+theorem pick2_zero {α β: Type} [Zero β]
+: ∀ (a: α), pick2 a (0: β) = (0: β) := by simp [pick2]
+
+theorem pick2_add {α β: Type} [AddZeroClass β]
+: ∀ (a: α) (b1 b2: β),
+pick2 a (b1+b2) = (pick2 a b1) + (pick2 a b2) := by simp [pick2]
+
 noncomputable def AccountSet.supply (as: AccountSet) (t: Token): NNReal :=
   (as.curried_swap t).sum pick2
 
-lemma AccountSet.supply_up_diff (as: AccountSet) (t: Token) (a: Account) (t': Token) (v: NNReal) (hdif: t ≠ t'):
+@[simp] lemma AccountSet.supply_up_diff (as: AccountSet) (t: Token) (a: Account) (t': Token) (v: NNReal) (hdif: t ≠ t'):
   supply (as.up a t' v) t = as.supply t := by 
   unfold supply
   rw [Finsupp.up_swap as a t' v]
   rw [Finsupp.up_diff _ _ _ _ _ hdif]
 
-noncomputable def AMMSet.supply (amms: AMMSet) (t: AtomicTok): NNReal :=
-  amms.map.sum (λ amm => 
-    if h:t ∈ amm.f.supp
-    then amm.r0 ⟨t, h⟩
-    else 0)
+noncomputable def AMMSet.supply (amms: AMMSet) (t: AtomicTok): NNReal := (amms.f t).sum λ _ x => x.fst
 
 noncomputable def State.supply (s: State) (t: Token): NNReal
   := match t with
   | Token.Atomic t' => (s.accs.supply t) + (s.amms.supply t')
   | Token.Minted _ => s.accs.supply t
 
-noncomputable def State.redeemrate
-  (s: State) (m: MintedOf s.amms) (t: AtomicOfM m): NNReal
-  := (m.amm.r0 t.ofAMM) / (s.supply m.m)
+
+theorem AccountSet.supply_addb_same 
+(as: AccountSet) (t: Token) (a: Account) (x: NNReal)
+: (as.addb a t x).supply t = x + (as.supply t) := by
+
+  simp [supply, addb, Finsupp.up_swap]
+  simp [Finsupp.up_eq]
+  rw [Finsupp.update_eq_single_add_erase]
+
+  rw [@Finsupp.sum_add_index' _ _ _ _ _ _ _ pick2 pick2_zero pick2_add]
+  rw [Finsupp.sum_single_index (pick2_zero a)]
+  rw [pick2_add]
+  rw [add_comm (pick2 a (as a t))]
+  rw [add_assoc]
+  rw [← Finsupp.curried_swap_def as]
+  rw [Finsupp.add_sum_erase']
+  . simp [pick2]
+  . exact pick2_zero
