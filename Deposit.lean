fb31fb7995ddab3d7c8653359aff82f043a4f2b3
Daniele Pusceddu <43929743+danielepusceddu@users.noreply.github.com>
2023-05-21 15:34:14 +0200

    Deposit: redefine using new AMMSets

--- Deposit.lean
+++ Deposit.lean
@@ -1,5 +1,6 @@
 import «AMMSet»
 import «State»
+import «Supply»
 
 namespace Deposit0
 
@@ -10,22 +11,27 @@ structure Valid (s: State) where
   r1: ℝ+
   a: Account
   hdif: t0 ≠ t1
-  hnin: ⟨{t0,t1}, by aesop⟩ ∉ s.amms.map.supp
+  hnin: s.amms.f t0 t1 = (0,0)
   hen0: (s.accs a) t0 ≤ r0
   hen1: (s.accs a) t1 ≤ r1 
-
-def amm {s: State} (v: Valid s): AMM :=
-  ⟨
-  (PFun.empty.update v.t0 v.r0).update v.t1 v.r1, by 
-  simp [PFun.update, PFun.empty];
-  exact Finset.card_doubleton v.hdif.symm
-  ⟩
 
 noncomputable def wallet {s: State} (v: Valid s): Wallet :=
   ((s.accs v.a).update v.t0 (((s.accs v.a) v.t0) - v.r0)).update v.t1 (((s.accs v.a) v.t1) - v.r1)
 
 noncomputable def apply {s: State} (v: Valid s): State :=
   ⟨
-  s.accs.update v.a (wallet v),
-  s.amms.update (amm v)
+  ((s.accs.subb v.a v.t0 v.r0).subb v.a v.t1 v.r1).addb v.a (AtomicTok.toMint v.hdif) v.r0,
+  s.amms.up v.t0 v.t1 (v.r0, v.r1) v.hdif (by sorry)
   ⟩
+
+@[simp] theorem supply_minted_diff {s: State} (v: Valid s)
+(m: MintedTok) (hdif: m ≠ (AtomicTok.toMint v.hdif)):
+(apply v).supply m = s.supply m := by
+  simp [apply, State.supply, AccountSet.addb]
+  simp [hdif, AccountSet.subb]
+
+@[simp] theorem diff_same {s: State} (v: Valid s)
+(t0 t1: AtomicTok) (hdif: (t0 ≠ v.t0 ∨ t1 ≠ v.t1) ∧ (t0 ≠ v.t1 ∨ t1 ≠ v.t0)):
+(apply v).amms.f t0 t1 = s.amms.f t0 t1 := by
+  simp [apply, State.supply, AccountSet.addb]
+  simp [hdif]
