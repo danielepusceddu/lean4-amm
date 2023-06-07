1455afb56ac0aeb422edffd062e4d3e4db787a3d
Daniele Pusceddu <43929743+danielepusceddu@users.noreply.github.com>
2023-06-07 16:41:32 +0200

    Define swaprate properties

--- AMMLib/State.lean
+++ AMMLib/State.lean
@@ -16,3 +16,38 @@ structure Γ where
   atoms: 𝕊₀
   mints: 𝕊₁
   amms: 𝕊ₐ
+
+def SX.outputbound (sx: SX): Prop :=
+  ∀ (x r0 r1: ℝ+),
+     x*(sx x r0 r1) < r1
+
+def SX.mono (sx: SX): Prop :=
+∀ (x r0 r1 x' r0' r1': ℝ+),
+  x' ≤ x ∧ r0' ≤ r0 ∧ r1' ≤ r1
+  →
+  sx x r0 r1 ≤ sx x' r0' r1'
+
+def SX.strictmono (sx: SX): Prop :=
+∀ (x r0 r1 x' r0' r1': ℝ+),
+  x' ≤ x ∧ r0' ≤ r0 ∧ r1' ≤ r1
+  →
+  if x' < x ∨ r0' < r0 ∨ r1' < r1
+  then sx x r0 r1 < sx x' r0' r1'
+  else sx x r0 r1 ≤ sx x' r0' r1'
+
+def SX.additive (sx: SX): Prop :=
+∀ (x y r0 r1: ℝ+),
+  sx (x+y) r0 r1
+  =
+  (x*(sx x r0 r1) + y*(sx y r0 r1))/(x + y)
+
+def SX.reversible 
+(sx: SX) (bound: sx.outputbound): Prop :=
+  ∀ (x r0 r1: ℝ+),
+    1 / (sx x r0 r1) = 
+    sx ((sx x r0 r1)*x)
+       (r1.sub (x*(sx x r0 r1)) (bound x r0 r1))
+       (r0 + x)
+
+def SX.homogeneous (sx: SX): Prop :=
+∀ (a x r0 r1: ℝ+), sx (a*x) (a*r0) (a*r1) = sx x r0 r1
