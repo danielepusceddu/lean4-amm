51f608767152be791f20fb163f7c80e4c2a34ab9
Daniele Pusceddu <43929743+danielepusceddu@users.noreply.github.com>
2023-06-08 12:22:20 +0200

    PReals: PReals as CommGroup + lemmas

--- HelpersLib/PReal.lean
+++ HelpersLib/PReal.lean
@@ -57,3 +57,74 @@ lemma coe_sub (x y: ℝ+) (gt: y < x):
 
 lemma coe_div (x y: ℝ+):
   (((x/y): ℝ+): ℝ)= (x:ℝ)/(y:ℝ) := by rfl
+
+lemma coe_le (x y: ℝ+):
+  x ≤ y ↔ (x: ℝ) ≤ (y: ℝ) := by rfl
+
+lemma coe_lt (x y: ℝ+):
+  x < y ↔ (x: ℝ) < (y: ℝ) := by rfl
+
+lemma coe_inv (x: ℝ+):
+  ((x⁻¹): ℝ+) = (x: ℝ)⁻¹ := by rfl
+
+lemma coe_eq' (x y: ℝ+):
+  x = y ↔ (x: ℝ) = (y: ℝ) := by 
+  apply Iff.intro
+  . intro heq
+    rw [heq]
+  . intro heq
+    rw [← x.val_eq_coe] at heq
+    rw [← y.val_eq_coe] at heq
+    rcases x with ⟨xval,xprop⟩ 
+    rcases y with ⟨yval,yprop⟩ 
+    simp at heq
+    simp [heq]
+
+noncomputable instance: Group ℝ+ where
+  mul_left_inv := by 
+    intro x
+    simp [coe_eq', coe_mul, coe_inv, x.coe_ne_zero]
+    rfl
+
+noncomputable instance: CommGroup ℝ+ where
+  mul_comm := by
+    intro x y
+    simp [PReal.coe_eq', coe_mul, mul_comm]
+
+instance: RightDistribClass ℝ+ where
+  right_distrib := by
+    intro x y z
+    simp only [coe_eq', coe_mul, coe_add]
+    exact right_distrib (x: ℝ) y z
+
+noncomputable instance: OrderedCommGroup ℝ+ where
+  mul_le_mul_left := by 
+    intro a b
+    intro hle
+    intro c
+    rw [mul_le_mul_iff_left]
+    exact hle
+
+theorem add_div'' {α: Type} 
+  [DivInvMonoid α] [Add α] [RightDistribClass α] 
+  (a b c: α):
+  (a + b) / c = a/c + b/c := 
+    by simp_rw [div_eq_mul_inv, add_mul]
+
+theorem lt_add_right (x y: ℝ+):
+  x < x+y := by
+  simp only [coe_lt, coe_add]
+  conv => lhs; rw [← zero_add (x: ℝ)]; rfl
+  rw [add_comm (x: ℝ)]
+  exact add_lt_add_right y.coe_pos x
+
+theorem gt_add_right (x y: ℝ+):
+  x+y > x := by
+  simp only [gt_iff_lt]
+  exact lt_add_right x y
+
+theorem div_lt_add_denum (x y z: ℝ+):
+  x/(y+z) < x/y := by
+  simp only [div_eq_mul_inv, mul_lt_mul_iff_left]
+  apply inv_lt_inv'
+  exact lt_add_right y z
