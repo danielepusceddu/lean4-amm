4295a5fcc3ced79d533a3ce6fd744e23fb0a0861
Daniele Pusceddu <43929743+danielepusceddu@users.noreply.github.com>
2023-11-17 18:51:53 +0100

    Fix error in strictmono def

--- AMMLib/Swap/Rate.lean
+++ AMMLib/Swap/Rate.lean
@@ -16,9 +16,9 @@ def SX.mono (sx: SX): Prop :=
 
 def SX.strictmono (sx: SX): Prop :=
 ∀ (x r0 r1 x' r0' r1': ℝ+),
-  x' ≤ x ∧ r0' ≤ r0 ∧ r1' ≤ r1
+  x' ≤ x ∧ r0' ≤ r0 ∧ r1 ≤ r1'
   →
-  if x' < x ∨ r0' < r0 ∨ r1' < r1
+  if x' < x ∨ r0' < r0 ∨ r1 < r1'
   then sx x r0 r1 < sx x' r0' r1'
   else sx x r0 r1 ≤ sx x' r0' r1'
 
